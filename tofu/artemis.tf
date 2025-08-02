acef97d795b7f611fe820904d37328e0c42479f2
Dave James Miller <d@djm.me>
2025-08-02 15:35:31 +0100

    Add artemis VM

---
+++ tofu/artemis.tf
@@ -0,0 +1,63 @@
+resource "hcloud_server" "artemis" {
+  name         = "artemis.djm.me"
+  server_type  = "cx22" # 2 vCPU (Intel), 4 GB RAM, 40 GB SSD, 20 TB traffic
+  image        = "debian-12"
+  location     = "hel1"
+  backups      = true
+  firewall_ids = [hcloud_firewall.web_server.id]
+  ssh_keys     = [hcloud_ssh_key.personal.id]
+
+  lifecycle {
+    ignore_changes = [ssh_keys]
+  }
+}
+
+resource "hcloud_rdns" "artemis_ipv4" {
+  server_id  = hcloud_server.artemis.id
+  ip_address = hcloud_server.artemis.ipv4_address
+  dns_ptr    = "artemis.djm.me"
+}
+
+resource "hcloud_rdns" "artemis_ipv6" {
+  server_id  = hcloud_server.artemis.id
+  ip_address = hcloud_server.artemis.ipv6_address
+  dns_ptr    = "artemis.djm.me"
+}
+
+resource "cloudflare_dns_record" "artemis_djm_me_A" {
+  zone_id = data.cloudflare_zone.djm_me.zone_id
+  name    = "artemis.djm.me"
+  type    = "A"
+  content = hcloud_server.artemis.ipv4_address
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "artemis_djm_me_AAAA" {
+  zone_id = data.cloudflare_zone.djm_me.zone_id
+  name    = "artemis.djm.me"
+  type    = "AAAA"
+  content = hcloud_server.artemis.ipv6_address
+  ttl     = 1
+}
+
+resource "aws_iam_user" "artemis_ses_postfix" {
+  name = "artemis-postfix-ses"
+}
+
+resource "aws_iam_user_group_membership" "artemis_ses_postfix_sending" {
+  user   = aws_iam_user.artemis_ses_postfix.name
+  groups = [data.aws_iam_group.ses_sending.group_name]
+}
+
+resource "aws_iam_access_key" "artemis_ses_postfix" {
+  user = aws_iam_user.artemis_ses_postfix.name
+}
+
+output "artemis_postfix_smtp_username" {
+  value = aws_iam_access_key.artemis_ses_postfix.id
+}
+
+output "artemis_postfix_smtp_password" {
+  value     = aws_iam_access_key.artemis_ses_postfix.ses_smtp_password_v4
+  sensitive = true
+}
