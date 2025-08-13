ba45a43a9d2b307a48d582c7b6aa3e581d84dba6
Dave James Miller <d@djm.me>
2025-08-13 19:17:01 +0100

    Fix quotes

--- ansible/roles/software_certbot/files/deploy.sh
+++ ansible/roles/software_certbot/files/deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 for service in apache2; do
     if [[ -f "/usr/lib/systemd/system/$service.service" ]]; then
-        echo 'Reloading $service...'
+        echo "Reloading $service..."
         systemctl reload "$service"
         systemctl status "$service"
     else
